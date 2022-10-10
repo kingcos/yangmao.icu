ff4c5a1cf91c15e5c253f2525f6a77fb1aeefec3
kingcos <2821836721v@gmail.com>
2022-10-11 01:11:21 +0800

    Showing main pages

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
-import { requestIssues } from "@/api/issues";
+import { requestActivities, type Activity } from "@/api/issues";
+import { reactive } from "vue";
 
-requestIssues("kingcos", "yangmao.icu")
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const data = reactive<{
+  activities: Activity[];
+}>({ activities: [] });
+
+requestActivities().then((result) => {
+  data.activities = result;
+});
 </script>
 
 <template>
-  <main>Main View</main>
+  <main>
+    <div class="activity" v-for="(activity, key) in data.activities" :key="key">
+      <div>地区：{{ activity.city }}</div>
+      <div>类型：{{ activity.type }}</div>
+      <div>要求：{{ activity.requirement }}</div>
+      <div>路径：{{ activity.steps }}</div>
+      <div>使用：{{ activity.usage }}</div>
+      <div>幅度：{{ activity.value }}</div>
+      <div>时间：{{ activity.time }}</div>
+      <div>
+        跳转：<a :href="activity.jump">{{ activity.requirement }}</a>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.activity {
+  margin-top: 20px;
+
+  &:nth-child(2n) {
+    background-color: aquamarine;
+  }
+  &:nth-child(2n + 1) {
+    background-color: bisque;
+  }
+}
+</style>
